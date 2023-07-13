07bc55d3f88934d6cd45d2259b3f215453769c7d
Re-Santos <renata.prog@gmail.com>
2023-07-13 11:31:25 -0300

    Cria Listagem de tarefas

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React from 'react';
 import './App.css';
 import { useState } from 'react';
+import {MdDelete} from 'react-icons/md';
 
 
 const App = () => {
   const ESCAPE_KEY = 27;
   const ENTER_KEY = 13;
+  
+  //Dentro do array, tem objetos, e esses objetos  tem propriedades
+  const initialTodos= [
+    { id: 1, title: 'Estudar JavaScript', checked: false},
+    { id: 2, title: 'Estudar React', checked: true},
+    { id: 3, title: 'Pausa', checked: false},
+  ]; //criando a lista de tarefas 1b (após criar, popular em use states em 1a)
 
+  const [todos, ]= useState(initialTodos) //Criando a lista de tarefas 1a
   const [value, setValue]= useState('');
+
 
   const erase = () =>{
     setValue('');
   };
 
+   // Por causa da função erase, quando digita algo no 'todo' da tarefa, ele envia e limpa o 'todo'.
   const submit = () =>{
-    console.log('submit',value);
+    console.log('submit', value);
     erase();
   };
 
+  //event, para receber o que a pessoa digitou
   const onChange = (event) => {
     setValue(event.target.value)
   };
 
+    //usando o parâmentro event, podemos receber os parâmetros do input
   const onKeyDown = (event)=>{
     if (event.which === ENTER_KEY){
      submit();
@@ -33,21 +46,33 @@ const App = () => {
   };
 
   return (
-  
- <section id = "app" className = 'container'>
-  <header>
-    <h1 className="title">Todo</h1>
-    </header>
-  <section className="main">
-    <input className="new-todo"
-     placeholder='Digite sua tarefa'
-     value={value}
-     onChange={onChange}
-     onKeyDown={onKeyDown}
-    />
-  </section>
- </section>
 
+    <section id = "app" className = 'container'>
+      <header>
+        <h1 className="title">Todo</h1>
+      </header>
+      <section className="main">
+        <input className="new-todo"
+        placeholder='Digite sua tarefa'
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        />
+        <ul className="todo-list">
+          {todos.map((todo)=>(
+            //cada elemento dentro do array, tem que ter o seu key, é o que diferencia esse elemento dos outros
+           <li key={todo.id.toString()}>
+            <span className='todo'>{todo.title}</span>
+           <button className='remove' type='button'>
+            <MdDelete size={28}/>
+           </button>
+           </li>
+           
+          ))}
+        </ul>
+      </section>
+    </section>
+  
   );
 }
 
